b91d38a8db6f1b3713ccc967c91abbb0028d961a
Abdulkerim Hıcıp <abdulkerimhcp@gmail.com>
2021-12-02 17:15:25 +0300

    make it better

---
+++ pages/order.jsx
@@ -0,0 +1,15 @@
+import Layout from "../components/layout";
+
+export default function Order() {
+
+    return (
+        <Layout>
+            <div className="container h-100">
+                <div className="row">
+                    <h1 className="mt-5 text-center">Siparişler</h1>
+                </div>
+            </div>
+        </Layout>
+    );
+}
+
